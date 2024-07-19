e8d2fa208036c786651b144880f9b470d3eece04
jeet0108 <jeetsabhadiya387@gmail.com>
2024-07-20 00:30:39 +0530

    Create Navbar

--- vite-project/src/components/Navbar.jsx
+++ vite-project/src/components/Navbar.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
-
+import React from "react";
+import { FcSearch, FcLike } from "react-icons/fc";
+import { IoCartOutline, IoSearch, IoHeartOutline } from "react-icons/io5";
 function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <div>
+      <div className="flex justify-between w-full px-48 bg-black">
+        <div className=" text-white py-2 text-center w-full">
+          Summer sale for all purchase and free delivery - 50%
+          <a href="" className="pl-2 underline">
+            ShopNow
+          </a>
+        </div>
+        <div>
+          <select
+            name="language"
+            id=""
+            className="bg-black text-white py-2 outline-none rounded-none border-none "
+          >
+            <option value="English">English</option>
+            <option value="French">French</option>
+            <option value="German">German</option>
+          </select>
+        </div>
+      </div>
+      <div className="flex w-full bg-white justify-between px-32 items-center py-2">
+        <div className="  font-bold text-xl">Exclusive</div>
+        <div className="">
+          <ul className=" flex justify-around gap-20 font-semibold">
+            <li><a href="">Home</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="">Contact</a></li>
+            <li><a href="">Sign up</a></li>
+          </ul>
+        </div>
+        <div className=" flex justify-around items-center gap-2">
+          <form className=" w-full flex items-center">
+            <input
+              type="text"
+              className=" border-2 px-2 w-64 py-2 rounded-sm border-none bg-gray-100"
+              placeholder=" What area you looking for ?"
+            ></input>
+            <button className=" relative right-8 ">
+              <IoSearch className=" text-xl" />
+            </button>
+          </form>
+
+          <div className=" flex items-center gap-5 font-bold text-2xl">
+            <IoHeartOutline />
+            <IoCartOutline />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
